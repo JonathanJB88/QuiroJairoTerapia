b70cba945a45808e0b9f138f7bd7560aff4bd931
Jonathan Bracho <jonajes0288@gmail.com>
2023-04-14 12:28:52 +0200

    [update]: refactored contact page

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,71 @@
+import autosize from 'autosize';
+
+interface ContactFormProps {
+  loading: boolean;
+  values: {
+    name: string;
+    email: string;
+    phone: string;
+    message: string;
+  };
+  handlers: {
+    handleChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+    handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  };
+}
+
+const inputClassName =
+  'w-full px-4 py-2 border rounded-md border-navy-blue focus:outline-none focus:ring-2 focus:ring-turquoise';
+
+export const ContactForm = ({
+  loading,
+  values: { name, email, phone, message },
+  handlers: { handleChange, handleSubmit },
+}: ContactFormProps) => {
+  return (
+    <form onSubmit={handleSubmit} className='space-y-4'>
+      <input
+        type='text'
+        name='name'
+        placeholder='Tu nombre'
+        value={name}
+        onChange={handleChange}
+        className={inputClassName}
+        required
+      />
+      <input
+        type='email'
+        name='email'
+        placeholder='Tu correo electrónico'
+        value={email}
+        onChange={handleChange}
+        className={inputClassName}
+        required
+      />
+      <input
+        type='tel'
+        name='phone'
+        placeholder='Tu número de teléfono'
+        value={phone}
+        onChange={handleChange}
+        className={inputClassName}
+        required
+      />
+      <textarea
+        ref={(el) => el && autosize(el)}
+        name='message'
+        placeholder='Escribe tu mensaje aquí...'
+        value={message}
+        onChange={handleChange}
+        className={`${inputClassName} h-32`}
+        required
+      />
+      <button
+        type='submit'
+        className='px-4 py-2 font-semibold transition-colors duration-300 border rounded-md text-navy-blue border-navy-blue hover:bg-navy-blue hover:text-white'
+      >
+        {loading ? 'Enviando tu mensaje...' : 'Envía tu mensaje'}
+      </button>
+    </form>
+  );
+};
---
+++ src/components/ContactItem.tsx
@@ -0,0 +1,24 @@
+import { cloneElement } from 'react';
+
+interface ContactItemProps {
+  icon: React.ReactElement;
+  type: 'mail' | 'whatsapp';
+  contact: string;
+}
+
+export const ANCHOR_CLASS =
+  'flex items-center justify-start text-navy-blue hover:text-turquoise transition-colors duration-300';
+
+export const ContactItem = ({ icon, type, contact }: ContactItemProps) => {
+  const link = type === 'mail' ? `mailto:${contact}` : `https://wa.me/${contact}`;
+  return (
+    <div className='font-sans text-sm italic md:text-base'>
+      <a href={link} className={ANCHOR_CLASS} target='_blank' rel='noopener noreferrer' aria-label={link}>
+        {cloneElement(icon, {
+          className: 'w-5 h-5 mx-1',
+        })}
+        <span className='font-medium'>{contact}</span>
+      </a>
+    </div>
+  );
+};
