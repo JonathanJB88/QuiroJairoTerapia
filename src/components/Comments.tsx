6f7a65e88fb3d874a844a9f1d324eea8d6000eca
Jonathan Bracho <jonajes0288@gmail.com>
2023-04-17 20:30:33 +0200

    [optimization]: commento styling

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,3 +1,28 @@
+// import { useEffect } from 'react';
+
+// export const Comments = () => {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.src = 'https://cdn.commento.io/js/commento.js';
+//     script.defer = true;
+//     script.setAttribute('data-auto-init', 'true');
+//     script.setAttribute('data-url', window.location.href);
+
+//     const container = document.getElementById('commento');
+//     container?.appendChild(script);
+
+//     return () => {
+//       container?.removeChild(script);
+//     };
+//   }, []);
+
+//   return (
+//     <div className='mt-8'>
+//       <div id='commento'></div>
+//     </div>
+//   );
+// };
+
 import { useEffect } from 'react';
 
 export const Comments = () => {
@@ -7,6 +32,7 @@ export const Comments = () => {
     script.defer = true;
     script.setAttribute('data-auto-init', 'true');
     script.setAttribute('data-url', window.location.href);
+    script.setAttribute('data-css-override', 'https://quirojairoterapia.vercel.app/css/commento-custom.css'); // Replace with the URL where your custom CSS file is hosted
 
     const container = document.getElementById('commento');
     container?.appendChild(script);
@@ -17,7 +43,7 @@ export const Comments = () => {
   }, []);
 
   return (
-    <div className='mt-8 commentContainer'>
+    <div className='mt-8'>
       <div id='commento'></div>
     </div>
   );
