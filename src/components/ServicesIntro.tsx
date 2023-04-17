14dff679d52bb598e3cd6dba5a4def30a32b406d
Jonathan Bracho <jonajes0288@gmail.com>
2023-04-17 17:34:23 +0200

    [add]: ServicesIntro component

---
+++ src/components/ServicesIntro.tsx
@@ -0,0 +1,15 @@
+export const ServicesIntro = () => {
+  return (
+    <>
+      <h2 className='mb-4 text-3xl font-extrabold md:text-4xl text-light-gray font-roboto text-shadow'>
+        Servicios de QuiroJairoTerapia
+      </h2>
+      <p className='mb-4 font-sans text-base md:text-lg text-light-gray text-shadow'>
+        ¡Descubre la magia del quiromasaje con QuiroJairoTerapia! Jairo, terapeuta profesional de quiromasaje, te invita
+        a disfrutar de una experiencia única y transformadora. Nuestros tratamientos están especialmente diseñados para
+        adaptarse a tus necesidades y preferencias, proporcionando un viaje de bienestar y relajación. Echa un vistazo a
+        nuestra amplia gama de masajes y terapias:
+      </p>
+    </>
+  );
+};
