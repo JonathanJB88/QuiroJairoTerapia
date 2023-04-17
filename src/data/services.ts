ef6ab79614c911ac00eae1c33c07da92b2650fdc
Jonathan Bracho <jonajes0288@gmail.com>
2023-04-17 17:34:59 +0200

    [update]: services data

--- src/data/services.ts
+++ src/data/services.ts
@@ -6,41 +6,48 @@ export const services: Iservice[] = [
     price: '40€/hora',
     description:
       'Este masaje te sumerge en un estado de relajación profunda, aliviando el estrés y la tensión acumulada. Ideal para desconectar y revitalizar cuerpo y mente.',
+    backgroundImageUrl: '/images/relajante.jpeg',
   },
   {
     title: 'Masaje Descontracturante',
     price: '40€/hora',
     description:
       'Perfecto para liberar tensiones musculares y contracturas, este masaje se centra en áreas problemáticas específicas para proporcionar alivio y restaurar la movilidad.',
+    backgroundImageUrl: '/images/descontracturante.jpeg',
   },
   {
     title: 'Masaje Circulatorio',
     price: '40€/hora',
     description:
       'Diseñado para mejorar la circulación sanguínea y linfática, este masaje estimula la eliminación de toxinas y ayuda a reducir la retención de líquidos.',
+    backgroundImageUrl: '/images/circulatorio.jpeg',
   },
   {
     title: 'Masaje Deportivo',
     price: '40€/hora',
     description:
       'Ideal para deportistas y personas activas, este masaje ayuda a prevenir lesiones, mejorar el rendimiento y acelerar la recuperación muscular.',
+    backgroundImageUrl: '/images/deportivo.jpeg',
   },
   {
     title: 'Reflexología Podal',
     price: '40€/hora',
     description:
       'Basado en la estimulación de puntos específicos en los pies, este tratamiento equilibra y armoniza la energía del cuerpo, promoviendo la auto-curación y el bienestar general.',
+    backgroundImageUrl: '/images/reflexologiapodal.jpeg',
   },
   {
     title: 'Masaje craneo-facial',
     price: '40€/hora',
     description:
       'Alivia tensiones en la cabeza, el cuello y el rostro mediante movimientos suaves y precisos. Ideal para combatir dolores de cabeza, migrañas y estrés.',
+    backgroundImageUrl: '/images/craneofacial.jpeg',
   },
   {
     title: 'Drenaje linfático',
     price: '40€/hora',
     description:
       'Este masaje suave y rítmico ayuda a eliminar el exceso de líquidos y toxinas del sistema linfático, mejorando la salud y fortaleciendo el sistema inmunológico.',
+    backgroundImageUrl: '/images/drenajelinfatico.jpeg',
   },
 ];
