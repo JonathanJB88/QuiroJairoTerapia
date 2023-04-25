7a5e5f5b67ac70f406ee6d2fa84da1eea18cd12b
Jonathan Bracho <jonajes0288@gmail.com>
2023-04-25 20:13:42 +0200

    [add]: Testimonial card - [optimization]: experiences component

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -1,13 +1,64 @@
-export const Experiencies = () => {
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import { testimonials } from '@/data';
+import { CustomArrow, SectionIntro, StarRating, TestimonialCard } from '@/components';
+import { useWindowSize } from '@/hooks';
+
+import { AiFillSafetyCertificate } from 'react-icons/ai';
+import { useState } from 'react';
+
+const title = 'Experiencias de QuiroJairoTerapia';
+const description =
+  'QuiroJairoTerapia ha ayudado a muchas personas a encontrar alivio y bienestar. Estas son algunas de las experiencias de nuestros clientes.';
+
+export const Experiences = () => {
+  const windowSize = useWindowSize();
+  const slidePercentage = windowSize.width >= 768 ? 33.33 : 100;
+  const averageRating = testimonials.reduce((sum, t) => sum + t.rating, 0) / testimonials.length;
+  const formattedAverage = averageRating.toFixed(1);
+
+  const [expandedTestimonialId, setExpandedTestimonialId] = useState<string>('');
+
+  const last50Testimonials = testimonials.slice(-50);
+
   return (
-    <div className='container px-4 mx-auto select-none md:px-8 lg:px-16'>
-      <h2 className='mb-4 text-3xl font-extrabold md:text-4xl text-light-gray font-roboto text-shadow'>
-        Experiencias de QuiroJairoTerapia
-      </h2>
-      <p className='mb-4 font-sans text-base md:text-lg text-light-gray text-shadow'>
-        QuiroJairoTerapia ha ayudado a muchas personas a encontrar alivio y bienestar. Estas son algunas de las
-        experiencias de nuestros clientes:
-      </p>
+    <div className='container px-4 py-16 mx-auto text-left select-none md:px-8 lg:px-16'>
+      <SectionIntro title={title} description={description} />
+      <div className='flex flex-row items-center mb-8 text-center md:text-left'>
+        <div className='inline-block mr-2 font-sans'>
+          <StarRating rating={averageRating} />
+        </div>
+        <div className='inline-block font-sans text-2xl font-bold text-light-gray'>{formattedAverage}</div>
+        <AiFillSafetyCertificate size={24} className='ml-2' />
+      </div>
+      <Carousel
+        className='mb-8 overflow-visible'
+        showThumbs={false}
+        showStatus={false}
+        showIndicators={false}
+        // autoPlay
+        // interval={3000}
+        infiniteLoop
+        useKeyboardArrows
+        stopOnHover
+        centerMode
+        centerSlidePercentage={slidePercentage}
+        renderArrowPrev={(clickHanler, hasPrev, label) => (
+          <CustomArrow clickHandler={clickHanler} hasArrow={hasPrev} label={label} />
+        )}
+        renderArrowNext={(clickHanler, hasNext, label) => (
+          <CustomArrow clickHandler={clickHanler} hasArrow={hasNext} label={label} />
+        )}
+      >
+        {last50Testimonials.map((testimonial) => (
+          <TestimonialCard
+            key={testimonial.commentId}
+            testimonial={testimonial}
+            expandedTestimonialId={expandedTestimonialId}
+            setExpandedTestimonialId={setExpandedTestimonialId}
+          />
+        ))}
+      </Carousel>
     </div>
   );
 };
