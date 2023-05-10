db0fa354b926f6a543dd925ea291e50f442ff78e
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-10 14:05:16 +0200

    [add]: authSlice

---
+++ src/store/auth/authSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface User {
+  uid: string;
+  name: string;
+}
+
+interface AuthState {
+  status: 'checking' | 'authenticated' | 'unauthenticated';
+  user: User | null;
+  errorMessage: string | undefined;
+}
+
+const initialState: AuthState = {
+  status: 'checking',
+  user: null,
+  errorMessage: undefined,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    onChecking: (state) => {
+      state.status = 'checking';
+      state.user = null;
+      state.errorMessage = undefined;
+    },
+    onLogin: (state, { payload }: PayloadAction<User>) => {
+      state.status = 'authenticated';
+      state.user = payload;
+      state.errorMessage = undefined;
+    },
+    onLogout: (state, { payload }: PayloadAction<string | undefined>) => {
+      state.status = 'unauthenticated';
+      state.user = null;
+      state.errorMessage = payload;
+    },
+    cleanErrorMessage: (state) => {
+      state.errorMessage = undefined;
+    },
+  },
+});
+
+export const { onChecking, onLogin, onLogout, cleanErrorMessage } = authSlice.actions;
