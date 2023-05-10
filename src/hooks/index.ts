b70a4035f6d5a359305775b19820ec9209b6928f
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-10 14:05:05 +0200

    [add]: useAuthStore hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
 export * from './useSmoothScroll';
 export * from './useContact';
 export * from './useWindowSize';
+export * from './useAuthStore';
+export * from './useForm';
---
+++ src/hooks/useAuthStore.ts
@@ -0,0 +1,94 @@
+import { useDispatch, useSelector } from 'react-redux';
+import apiClient from '@/helpers/apiConfig';
+import { RootState } from '@/store';
+import { onChecking, onLogin, onLogout, cleanErrorMessage } from '@/store';
+import axios from 'axios';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+interface RegisterData extends LoginData {
+  name: string;
+}
+
+export const useAuthStore = () => {
+  const { status, user, errorMessage } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+
+  const handleErrorMessage = (message: string) => {
+    dispatch(onLogout(message));
+    setTimeout(() => {
+      dispatch(cleanErrorMessage());
+    }, 3000);
+  };
+
+  const login = async ({ email, password }: LoginData) => {
+    dispatch(onChecking());
+    try {
+      const { data } = await apiClient.post('/api/auth/login', { email, password });
+
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('token-init-date', new Date().getTime().toString());
+
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      const errorMessage =
+        axios.isAxiosError(error) && error.response?.data.errors.email.msg
+          ? error.response.data.errors.email.msg
+          : 'Usuario/Contraseña inválidos';
+      handleErrorMessage(errorMessage);
+    }
+  };
+
+  const register = async ({ name, email, password }: RegisterData) => {
+    dispatch(onChecking());
+    try {
+      const { data } = await apiClient.post('/api/auth/signup', { name, email, password });
+
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('token-init-date', new Date().getTime().toString());
+
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      const errorMessage =
+        axios.isAxiosError(error) && error.response?.data.msg ? error.response.data.msg : 'Error al crear usuario';
+      handleErrorMessage(errorMessage);
+    }
+  };
+
+  const checkAuthToken = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) return dispatch(onLogout());
+
+    try {
+      const { data } = await apiClient.get('/api/auth/renewToken');
+
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('token-init-date', new Date().getTime().toString());
+
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      localStorage.clear();
+      dispatch(onLogout());
+    }
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    dispatch(onLogout());
+  };
+
+  return {
+    //Props
+    status,
+    user,
+    errorMessage,
+    //Functions
+    login,
+    register,
+    checkAuthToken,
+    logout,
+  };
+};
