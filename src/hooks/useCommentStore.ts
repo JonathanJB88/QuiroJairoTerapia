fe6e772ac3044b0bf022758a7b5faa4050d2111c
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-12 10:43:52 +0200

    [update]: better types adapting to new response

--- src/hooks/useCommentStore.ts
+++ src/hooks/useCommentStore.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 import apiClient from '@/helpers/apiConfig';
 import {
   RootState,
-  Comment,
   onLoading,
   onGetComments,
   onPostComment,
@@ -11,15 +10,15 @@ import {
   onDeleteComment,
   onFailed,
   onCleanErrorMessage,
+  Comment,
 } from '@/store';
 import { CommentType } from '@/interfaces';
+import { CommentToPost, CommentToUpdate } from '@/components';
 
-export interface CommentPost {
-  userId: string;
-  content: string;
-  rating: number;
-  type: CommentType;
-  postId?: string;
+interface CommentResponse {
+  ok: boolean;
+  msg: string;
+  comment: Comment;
 }
 
 export const useCommentStore = () => {
@@ -40,7 +39,7 @@ export const useCommentStore = () => {
         type === 'review'
           ? `/api/comments/get-by-post?type=${type}`
           : `/api/comments/get-by-post?type=${type}&postId=${postId}`;
-      const { data } = await apiClient.get(endpoint);
+      const { data } = await apiClient.get<Comment[]>(endpoint);
       dispatch(onGetComments(data));
     } catch (error) {
       const errorMessage =
@@ -51,45 +50,61 @@ export const useCommentStore = () => {
     }
   };
 
-  const postComment = async (comment: CommentPost) => {
+  const postComment = async (comment: CommentToPost): Promise<{ ok: boolean; msg: string }> => {
     dispatch(onLoading());
     try {
-      const { data } = await apiClient.post('/api/comments/create', comment);
-      dispatch(onPostComment(data));
+      const {
+        data: { comment: postedComment, ok, msg },
+      } = await apiClient.post<CommentResponse>('/api/comments/create', comment);
+      dispatch(onPostComment(postedComment));
+      return { ok, msg };
     } catch (error) {
       const errorMessage =
         axios.isAxiosError(error) && error.response?.data.msg
           ? error.response.data.msg
           : 'Error al crear el comentario';
       handleErrorMessage(errorMessage);
+      return { ok: false, msg: errorMessage };
     }
   };
 
-  const updateComment = async (comment: Comment) => {
+  const updateComment = async (comment: CommentToUpdate): Promise<{ ok: boolean; msg: string }> => {
     dispatch(onLoading());
     try {
-      const { data } = await apiClient.put('/api/comments/update', comment);
-      dispatch(onUpdateComment(data));
+      const {
+        data: { comment: updatedComment, ok, msg },
+      } = await apiClient.put<CommentResponse>('/api/comments/update', comment);
+      dispatch(onUpdateComment(updatedComment));
+      return { ok, msg };
     } catch (error) {
       const errorMessage =
         axios.isAxiosError(error) && error.response?.data.msg
           ? error.response.data.msg
           : 'Error al actualizar el comentario';
       handleErrorMessage(errorMessage);
+      return { ok: false, msg: errorMessage };
     }
   };
 
-  const deleteComment = async (commentId: string) => {
+  const deleteComment = async (commentId: string): Promise<{ ok: boolean; msg: string }> => {
     dispatch(onLoading());
     try {
-      await apiClient.delete(`/api/comments/delete?commentId=${commentId}`);
-      dispatch(onDeleteComment(commentId));
+      const {
+        data: {
+          comment: { commentId: deletedCommentId },
+          ok,
+          msg,
+        },
+      } = await apiClient.delete<CommentResponse>(`/api/comments/delete?commentId=${commentId}`);
+      dispatch(onDeleteComment(deletedCommentId));
+      return { ok, msg };
     } catch (error) {
       const errorMessage =
         axios.isAxiosError(error) && error.response?.data.msg
           ? error.response.data.msg
           : 'Error al eliminar el comentario';
       handleErrorMessage(errorMessage);
+      return { ok: false, msg: errorMessage };
     }
   };
 
