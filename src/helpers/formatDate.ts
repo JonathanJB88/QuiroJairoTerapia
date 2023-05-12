c9105c024c7fc5601599678d63f863e666ba441f
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-12 10:42:05 +0200

    [update]: when 0 days, better to render hoy

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -3,9 +3,12 @@ export const formatDate = (date: Date) => {
   const createdDate = new Date(date);
   const daysDifference = Math.floor((currentDate.getTime() - createdDate.getTime()) / (1000 * 3600 * 24));
 
-  if (daysDifference < 5) {
-    return `hace ${daysDifference} días`;
-  } else {
-    return createdDate.toLocaleDateString('es-ES', { day: 'numeric', month: 'short', year: 'numeric' });
-  }
+  const dateString =
+    daysDifference === 0
+      ? 'Hoy'
+      : daysDifference < 5
+      ? `hace ${daysDifference} días`
+      : createdDate.toLocaleDateString('es-ES', { day: 'numeric', month: 'short', year: 'numeric' });
+
+  return dateString;
 };
