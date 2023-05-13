50ee2e2d512bd7ef65b4e2c0781b72300cc551dd
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-13 15:12:35 +0200

    [optimization]: useForm to get the desired behavior

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,7 +1,11 @@
 import { useEffect, useMemo, useState } from 'react';
 
-type FormValidators<T> = {
-  [key in keyof T]?: [(value: T[key]) => boolean, string];
+export type FormValidators<T> = {
+  [key in keyof T]?: {
+    validator: (value: T[key], state: T) => boolean;
+    message: string;
+    dependentFields?: (keyof T)[];
+  };
 };
 
 type FormState<T> = {
@@ -12,14 +16,10 @@ export const useForm = <T extends FormState<T>>(
   initialForm: T = {} as T,
   formValidations: FormValidators<T> = {} as FormValidators<T>
 ) => {
-  const [formState, setFormState] = useState<T>(initialForm);
+  const [formState, setFormState] = useState<T>({ ...initialForm });
   const [formValidation, setFormValidation] = useState<Partial<Record<keyof T, string | null>>>(
     {} as Partial<Record<keyof T, string | null>>
   );
-
-  useEffect(() => {
-    createValidators();
-  }, [formState]);
 
   useEffect(() => {
     setFormState(initialForm);
@@ -29,32 +29,39 @@ export const useForm = <T extends FormState<T>>(
     for (const formValue of Object.keys(formValidation)) {
       if (formValidation[formValue as keyof T] !== null) return false;
     }
-
     return true;
   }, [formValidation]);
 
   const onInputChange = ({ target }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = target;
-    setFormState({
-      ...formState,
-      [name as keyof T]: value,
+    setFormState((prevState) => {
+      const updatedState = { ...prevState, [name as keyof T]: value };
+
+      if (formValidations[name as keyof T]?.dependentFields) {
+        for (const dependentField of formValidations[name as keyof T]!.dependentFields!) {
+          const { validator, message } = formValidations[dependentField]!;
+          setFormValidation((prevValidationState) => ({
+            ...prevValidationState,
+            [dependentField]: validator(updatedState[dependentField], updatedState) ? null : message,
+          }));
+        }
+      }
+
+      return updatedState;
     });
+
+    if (formValidations[name as keyof T]) {
+      const { validator, message } = formValidations[name as keyof T]!;
+      setFormValidation((prevValidationState) => ({
+        ...prevValidationState,
+        [name as keyof T]: validator(value as T[keyof T], formState) ? null : message,
+      }));
+    }
   };
 
   const onResetForm = () => {
     setFormState(initialForm);
-  };
-
-  const createValidators = () => {
-    const formCheckedValues: Partial<Record<keyof T, string | null>> = {};
-
-    for (const formField of Object.keys(formValidations)) {
-      const [fn, errorMessage] = formValidations[formField as keyof T]!;
-
-      formCheckedValues[`${formField}Valid` as keyof T] = fn(formState[formField as keyof T]) ? null : errorMessage;
-    }
-
-    setFormValidation(formCheckedValues);
+    setFormValidation({} as Partial<Record<keyof T, string | null>>);
   };
 
   return {
@@ -65,6 +72,7 @@ export const useForm = <T extends FormState<T>>(
     onResetForm,
 
     ...formValidation,
+    formValidation,
     isFormValid,
   };
 };
