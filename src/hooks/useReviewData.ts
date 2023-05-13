aaefca11a24843654b9cf8caa26fa6c8f2dc45f3
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-13 15:11:00 +0200

    [add]: useReviewData hook

---
+++ src/hooks/useReviewData.ts
@@ -0,0 +1,20 @@
+import { UserRole } from '@/models/User';
+import { Comment, User } from '@/store';
+import { useMemo } from 'react';
+
+export const useReviewData = (user: User | null, comments: Comment[]) => {
+  const averageRating = useMemo(() => {
+    const totalRating = comments.reduce((sum, c) => sum + c.rating, 0);
+    return comments.length ? totalRating / comments.length : 0;
+  }, [comments]);
+
+  const formattedAverage = useMemo(() => averageRating.toFixed(1), [averageRating]);
+
+  const filteredComments = useMemo(() => {
+    return user && user.role === UserRole.ADMIN ? comments : comments.filter((c) => c.approved);
+  }, [user, comments]);
+
+  const recentReviews = useMemo(() => filteredComments.slice(0, 50), [filteredComments]);
+
+  return { averageRating, formattedAverage, filteredComments, recentReviews };
+};
