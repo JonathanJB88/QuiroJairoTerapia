462e705db957878a072101d2458197c1d525156d
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-20 21:20:51 +0200

    [update]: moving to high order the carousel styles

--- src/components/Experiences/Experiences.tsx
+++ src/components/Experiences/Experiences.tsx
@@ -1,4 +1,3 @@
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { useEffect, useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import { AiFillSafetyCertificate } from 'react-icons/ai';
