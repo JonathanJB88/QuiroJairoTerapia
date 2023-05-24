d83a01dba1667c3e23b846e011c2c6f1bcbf0022
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-24 12:25:04 +0200

    [add]: PostImage component

---
+++ src/components/Blog/PostImage.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import { Post } from '@/interfaces';
+
+const imageSizes = '(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 100vw, 1280px';
+
+export const PostImage = ({ post }: { post: Post }) => (
+  <Image
+    src={post.mainImage?.asset.url}
+    alt={post.mainImage?.alt || `Imagen de portada del post ${post.title}`}
+    fill
+    sizes={imageSizes}
+    className='rounded-lg shadow-lg'
+  />
+);
