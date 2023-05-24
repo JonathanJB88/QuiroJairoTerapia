fc13149c4a7377b8cb2ad0d6b012d37d70b1bcf2
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-24 12:25:36 +0200

    [add]: PostLoading component

---
+++ src/components/Blog/PostsLoading.tsx
@@ -0,0 +1,7 @@
+export const PostsLoading = () => {
+  return (
+    <h4 className='px-1 font-sans text-center rounded-md w-60 text-turquoise bg-navy-blue'>
+      Scroll para ver más posts...
+    </h4>
+  );
+};
