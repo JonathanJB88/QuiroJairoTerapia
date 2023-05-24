92dc5932cd3915dc997e07454573f6bb1205d22f
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-24 12:27:39 +0200

    [optimization]: Blog section

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,87 +1,26 @@
-import { Post } from '@/interfaces';
-import { useState, useEffect } from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import { CarouselPostCard, InputField, SectionIntro } from '@/components';
+import { memo, useState } from 'react';
+import { AllBlogPosts, LatestPosts } from '@/components';
+import { Id, Post } from '@/interfaces';
 
 interface BlogProps {
   posts: Post[];
+  scrollToSection: (id: Id) => void;
 }
 
-export const Blog = ({ posts }: BlogProps) => {
-  // Search term state
-  const [searchTerm, setSearchTerm] = useState('');
+export const Blog = ({ posts, scrollToSection }: BlogProps) => {
+  const [showAll, setShowAll] = useState(false);
 
-  // Filtered and sorted posts
-  const [displayPosts, setDisplayPosts] = useState<Post[]>([]);
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+    scrollToSection('consejos');
+  };
 
-  // Sort and filter posts whenever posts or search term changes
-  useEffect(() => {
-    let newPosts = posts;
+  const MemoizedAllBlogPosts = memo(AllBlogPosts);
+  const MemoizedLatestPosts = memo(LatestPosts);
 
-    // Filter by search term (in real application, implement more sophisticated search)
-    if (searchTerm) {
-      newPosts = newPosts.filter((post) => post.title.toLowerCase().includes(searchTerm.toLowerCase()));
-    }
-
-    // Sort by date
-    // newPosts = newPosts.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
-
-    setDisplayPosts(newPosts);
-  }, [posts, searchTerm]);
-
-  // Render
-  return (
-    <div className='flex flex-col justify-center w-full p-4 min-vh-screen'>
-      <SectionIntro
-        title='Toma el Control de Tu Bienestar:'
-        description='Últimas Reflexiones y Consejos de Tu Terapeuta'
-      />
-
-      <Carousel
-        showThumbs={false}
-        showStatus={false}
-        showIndicators
-        autoPlay
-        interval={5000}
-        infiniteLoop
-        useKeyboardArrows
-        swipeable
-        emulateTouch
-        showArrows={false}
-        stopOnHover
-        dynamicHeight
-      >
-        {posts.slice(-5).map((post) => (
-          <CarouselPostCard key={post.slug} post={post} />
-        ))}
-      </Carousel>
-
-      <div className='w-full mt-4'>
-        <InputField
-          name='searchTerm'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder='Buscar...'
-          type='text'
-          error={undefined}
-        />
-
-        <div className='grid grid-cols-1 gap-4 mt-4 md:grid-cols-2 lg:grid-cols-3'>
-          {displayPosts.map((post) => (
-            <div key={post.slug} className='overflow-hidden bg-white rounded-lg shadow-md'>
-              <img className='object-cover w-full h-56' src={post.mainImage.asset.url} alt={post.mainImage.alt} />
-              <div className='p-4'>
-                <h3 className='mb-2 text-lg font-semibold font-roboto text-navy-blue'>{post.title}</h3>
-                <div className='flex items-center mb-4 text-sm text-navy-blue-lighter'>
-                  <p>{post.authorName}</p>
-                  <p className='ml-4'>{post.publishedAt}</p>
-                </div>
-                <button className='font-semibold text-turquoise hover:text-turquoise-lighter'>Leer más</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+  return showAll ? (
+    <MemoizedAllBlogPosts handleLatestPosts={toggleShowAll} posts={posts} />
+  ) : (
+    <MemoizedLatestPosts handleAllPosts={toggleShowAll} posts={posts} />
   );
 };
