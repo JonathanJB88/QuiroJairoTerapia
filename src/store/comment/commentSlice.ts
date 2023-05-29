8806d861984e08b1fa01584051ef075c3fbd0608
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-29 17:32:32 +0200

    [optimization]: rename of prop

--- src/store/comment/commentSlice.ts
+++ src/store/comment/commentSlice.ts
@@ -4,7 +4,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 export interface Comment {
   commentId: string;
   postId: string;
-  userId: {
+  user: {
     _id: string;
     name: string;
   };
