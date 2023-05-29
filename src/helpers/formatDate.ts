d23420452d7418fad13ae353ce3629fdda465d82
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-29 17:28:30 +0200

    [optimization]: refactor of the formatDate function.

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,14 +1,25 @@
-export const formatDate = (date: Date) => {
+export const formatDate = (date: Date, format: string = 'relative') => {
   const currentDate = new Date();
   const createdDate = new Date(date);
   const daysDifference = Math.floor((currentDate.getTime() - createdDate.getTime()) / (1000 * 3600 * 24));
 
-  const dateString =
-    daysDifference === 0
-      ? 'Hoy'
-      : daysDifference < 5
-      ? `hace ${daysDifference} días`
-      : createdDate.toLocaleDateString('es-ES', { day: 'numeric', month: 'short', year: 'numeric' });
+  if (format === 'relative') {
+    const dateString =
+      daysDifference === 0
+        ? 'Hoy'
+        : daysDifference < 5
+        ? `hace ${daysDifference} días`
+        : createdDate.toLocaleDateString('es-ES', { day: 'numeric', month: 'short', year: 'numeric' });
 
-  return dateString;
+    return dateString;
+  }
+  if (format === 'published') {
+    return `Publicado el ${createdDate.toLocaleDateString('es-ES', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    })}`;
+  }
+
+  return '';
 };
