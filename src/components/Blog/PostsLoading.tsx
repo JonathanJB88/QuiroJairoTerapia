8c1fe92b366abe0b917f5ee6238be06521723f81
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-29 17:25:36 +0200

    [add]: Scroll loading...

--- src/components/Blog/PostsLoading.tsx
+++ src/components/Blog/PostsLoading.tsx
@@ -1,6 +1,6 @@
 export const PostsLoading = () => {
   return (
-    <h4 className='px-1 font-sans text-center rounded-md w-60 text-turquoise bg-navy-blue'>
+    <h4 className='px-1 font-sans text-center rounded-md w-fit text-turquoise bg-navy-blue'>
       Scroll para ver más posts...
     </h4>
   );
