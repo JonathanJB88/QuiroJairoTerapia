e3bcaa3434612384d0b8591d8363b6c6021afd41
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-30 21:11:20 +0200

    [optimization]: better rendering for categories

--- src/components/Blog/PostCategories.tsx
+++ src/components/Blog/PostCategories.tsx
@@ -6,15 +6,16 @@ interface PostCategoriesProps {
 
 export const PostCategories = ({ categories }: PostCategoriesProps) => {
   return (
-    <div className='flex items-center justify-start mb-2'>
+    <div className='flex items-center justify-start mb-2 whitespace-nowrap'>
       <p className='px-2 py-1 font-bold uppercase rounded-md shadow-md text-2xs md:text-xs text-navy-blue bg-turquoise font-roboto'>
         Categoría
       </p>
-      {categories.map((category) => (
-        <span key={category._id} className='mx-2 text-xs md:text-sm text-light-gray text-shadow'>
-          {category.title}
-        </span>
-      ))}
+      <span key={categories[0]._id} className='mx-2 text-xs md:text-sm text-light-gray text-shadow'>
+        {categories[0].title}
+      </span>
+      {categories.length > 1 && (
+        <span className='mx-2 text-xs md:text-sm text-light-gray text-shadow'>+{categories.length - 1}</span>
+      )}
     </div>
   );
 };
