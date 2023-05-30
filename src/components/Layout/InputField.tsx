bd61471c5d72aaf662f12bbb112e57d47c841b41
Jonathan Bracho <jonajes0288@gmail.com>
2023-05-30 20:33:25 +0200

    [update]: Change the way to render validation errors.

--- src/components/Layout/InputField.tsx
+++ src/components/Layout/InputField.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { toastNotification } from '@/helpers';
+
 interface InputFieldProps {
   name: string;
   type: string;
@@ -11,20 +14,24 @@ interface InputFieldProps {
 
 const inputClassName = 'w-full p-2 font-sans border rounded-md focus:outline-none';
 
-export const InputField = ({ name, type, placeholder, value, onChange, error, minLength, size }: InputFieldProps) => (
-  <div className='mb-2'>
-    <input
-      type={type}
-      placeholder={placeholder}
-      className={`${inputClassName} ${
-        error ? 'border-red-500' : 'focus:ring-2 focus:ring-turquoise border-navy-blue focus:border-transparent'
-      } ${size === 'small' ? 'text-sm' : 'text-base'}`}
-      name={name}
-      value={value}
-      onChange={onChange}
-      required
-      minLength={minLength}
-    />
-    {error && <p className='p-1 font-sans text-xs text-red-500'>{error}</p>}
-  </div>
-);
+export const InputField = ({ name, type, placeholder, value, onChange, error, minLength, size }: InputFieldProps) => {
+  useEffect(() => {
+    if (error) return toastNotification('error', error);
+  }, [error]);
+  return (
+    <div className='mb-2'>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={`${inputClassName} ${
+          error ? 'border-red-500' : 'focus:ring-2 focus:ring-turquoise border-navy-blue focus:border-transparent'
+        } ${size === 'small' ? 'text-sm' : 'text-base'}`}
+        name={name}
+        value={value}
+        onChange={onChange}
+        required
+        minLength={minLength}
+      />
+    </div>
+  );
+};
